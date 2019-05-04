5151c6ffa8438e6252c52a8fd82fde7b82e93547
Dylan Moberg <dylan.moberg@concur.com>
2019-05-03 18:50:16 -0700

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: 'airbnb',
+    rules: {
+        indent: [1, 4],
+        "react/jsx-filename-extension": [0, { "extensions": [".js", ".jsx"] }],
+        "react/jsx-indent": [0, { "extensions": [".js", ".jsx"] }],
+        "react/jsx-indent-props": [0, { "extensions": [".js", ".jsx"] }]
+    }
+};
