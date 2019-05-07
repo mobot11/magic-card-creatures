981381446b33c7231373fcfa5dd6e0268481dadd
Dylan Moberg <dylan.moberg@concur.com>
2019-05-06 17:17:27 -0700

    finish api unit test

--- src/utils/getCards.test.js
+++ src/utils/getCards.test.js
@@ -1,12 +1,39 @@
 import fetch from 'node-fetch';
 import getCards from './getCards';
 
+jest.mock('node-fetch');
+const { Response } = jest.requireActual('node-fetch');
 
 describe('get cards', () => {
-    console.log(fetch);
     it('Should call fetch', async () => {
-        getCards(1, 20, 'Name');
+        fetch.mockReturnValue(Promise.resolve(new Response(JSON.stringify({ cards: [] }))));
 
-        await expect(fetch).toHaveBeenCalledTimes(1);
+        await getCards(20, 1, 'name');
+
+        expect(fetch).toHaveBeenCalled();
+    });
+    it('Should call fetch with the correct query string', async () => {
+        fetch.mockReturnValue(Promise.resolve(new Response(JSON.stringify({ cards: [] }))));
+
+        await getCards(20, 1, 'name');
+
+        expect(fetch).toHaveBeenCalledWith(
+            'https://api.magicthegathering.io/v1/cards?type=creature&pageSize=20&page=1&contains=imageUrl&orderBy=name',
+        );
+    });
+    it('Should only return cards with images', async () => {
+        fetch.mockReturnValue(
+            Promise.resolve(
+                new Response(
+                    JSON.stringify({
+                        cards: [{ name: 'test', imageUrl: 'mycoolimage.jpg' }, { name: 'test2' }],
+                    }),
+                ),
+            ),
+        );
+
+        const cards = await getCards(20, 1, 'name');
+
+        expect(cards.length).toBe(1);
     });
 });
